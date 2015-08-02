f65a76d1146ba5d25dfa56cf2eb57aa16c97e648
mjhassanpur <mjhassanpur@gmail.com>
2015-08-02 15:48:18 -0700

    Retain current book details until new valid ISBN is entered

--- alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -71,8 +71,7 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
                 if(ean.length()==10 && !ean.startsWith("978")){
                     ean="978"+ean;
                 }
-                if(ean.length()<13){
-                    clearFields();
+                if (ean.length() != 13) {
                     return;
                 }
                 //Once we have an ISBN, start a book intent
